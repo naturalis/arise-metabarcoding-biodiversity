7cba6754d3b14802f997024ec3743c5a473b8e58
winnywinbin <62395514+winnywinbin@users.noreply.github.com>
2022-11-01 12:24:26 +0100

    Primers verwijderd in test dataset

--- Results/DADA2.R
+++ Results/DADA2.R
@@ -5,6 +5,11 @@ packageVersion("ShortRead")
 library(Biostrings)
 packageVersion("Biostrings")
 
+if (!require("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("ShortRead")
+
 #path <- "/Users/winnythoen/Library/CloudStorage/GoogleDrive-winnywinbin@gmail.com/.shortcut-targets-by-id/1E5lAF2Q142BBy6yXw-NM1VRV6_5N3le3/21022-375801_ARISE_Soil_Pilot/BaseClear/145561_21022_NovaseqSoilMetabarcoding _nafiesa/raw_sequences"
 path <- "/Users/winnythoen/Desktop/BioInformatica/Afstuderen/TestData"
 list.files(path)
@@ -15,13 +20,6 @@ fnRs <- sort(list.files(path, pattern = "_R2_", full.names = TRUE))
 plotQualityProfile(fnFs[1:2])
 plotQualityProfile(fnRs[1:2])
 
-# Install cutadapt for removing the primers: http://cutadapt.readthedocs.io/en/stable/index.html
-
-errF <- learnErrors(fnFs, multithread = TRUE)
-
-cutadapt <- "/Users/winnythoen/opt/anaconda3/bin/cutadapt"
-system2(cutadapt, args = "--version")
-
 # Identify the primers
 
 FWD1 <- "CTAGACTCGTCATCGATGAAGAACGCAG"
@@ -29,6 +27,7 @@ FWD2 <- "CTAGACTCGTCAACGATGAAGAACGCAG"
 FWD3 <- "CTAGACTCGTCACCGATGAAGAACGCAG"
 FWD4 <- "CTAGACTCGTCATCGATGAAGAACGTAG"
 FWD5 <- "CTAGACTCGTCATCGATGAAGAACGTGG"
+REV <- "TCCTSCGCTTATTGATATGC"
 
 allOrients <- function(primer) {
   require(Biostrings)
@@ -44,11 +43,16 @@ FWD3.orients <- allOrients(FWD3)
 FWD4.orients <- allOrients(FWD4)
 FWD5.orients <- allOrients(FWD5)
 FWD5.orients
+REV.orients <- allOrients(REV)
+
 
 fnFs.filtN <- file.path(path, "filtN", basename(fnFs))
 fnRs.filtN <- file.path(path, "filtN", basename(fnRs))
 filterAndTrim(fnFs, fnFs.filtN, fnRs, fnRs.filtN, maxN = 0, multithread = TRUE)
 # Hier krijg ik nu een error, misschien opnieuw na het verwijderen van de primers
+
+fnFs.filtN <- file.path("/Users/winnythoen/Desktop/BioInformatica/Afstuderen/TestData/filtN", basename(fnFs))
+fnRs.filtN <- file.path("/Users/winnythoen/Desktop/BioInformatica/Afstuderen/TestData/filtN", basename(fnRs))
 
 primerHits <- function(primer, fn) {
   nhits <- vcountPattern(primer, sread(readFastq(fn)), fixed = FALSE)
@@ -64,15 +68,48 @@ rbind(FWD1.ForwardReads = sapply(FWD1.orients, primerHits, fn = fnFs.filtN[[1]])
       FWD4.ForwardReads = sapply(FWD4.orients, primerHits, fn = fnFs.filtN[[1]]),
       FWD4.ReverseReads = sapply(FWD4.orients, primerHits, fn = fnRs.filtN[[1]]),
       FWD5.ForwardReads = sapply(FWD5.orients, primerHits, fn = fnFs.filtN[[1]]),
-      FWD5.ReverseReads = sapply(FWD5.orients, primerHits, fn = fnRs.filtN[[1]]))
+      FWD5.ReverseReads = sapply(FWD5.orients, primerHits, fn = fnRs.filtN[[1]]),
+      REV.ForwardReads = sapply(REV.orients, primerHits, fn = fnFs.filtN[[1]]),
+      REV.ReverseReads = sapply(REV.orients, primerHits, fn = fnRs.filtN[[1]]))
+
+
+# Install cutadapt for removing the primers: http://cutadapt.readthedocs.io/en/stable/index.html
+
+cutadapt <- "/Users/winnythoen/opt/anaconda3/bin/cutadapt"
+system2(cutadapt, args = "--version")
 
 path.cut <- file.path(path, "cutadapt")
 if(!dir.exists(path.cut)) dir.create(path.cut)
 fnFs.cut <- file.path(path.cut, basename(fnFs))
-fnFs.cut <- file.path(path.cut, basename(fnFs))
+fnRs.cut <- file.path(path.cut, basename(fnRs))
 
 FWD1.RC <- dada2:::rc(FWD1)
 FWD2.RC <- dada2:::rc(FWD2)
 FWD3.RC <- dada2:::rc(FWD3)
 FWD4.RC <- dada2:::rc(FWD4)
 FWD5.RC <- dada2:::rc(FWD5)
+REV.RC <- dada2::rc(REV)
+# Trim FWD and the reverse-complement of REV off of R1 (forward reads)
+R1.flags <- paste("-g", FWD1, "-a", REV.RC) 
+# Trim REV and the reverse-complement of FWD off of R2 (reverse reads)
+R2.flags <- paste("-G", REV, "-A", FWD1.RC) 
+
+for(i in seq_along(fnFs)) {
+  system2(cutadapt, args = c(R1.flags, R2.flags, "-n", 2, # -n 2 required to remove FWD and REV from reads
+                             "-o", fnFs.cut[i], "-p", fnRs.cut[i], # output files
+                             fnFs.filtN[i], fnRs.filtN[i])) # input files
+}
+
+rbind(FWD1.ForwardReads = sapply(FWD1.orients, primerHits, fn = fnFs.cut[[1]]),
+      FWD1.ReverseReads = sapply(FWD1.orients, primerHits, fn = fnRs.cut[[1]]),
+      FWD2.ForwardReads = sapply(FWD2.orients, primerHits, fn = fnFs.cut[[1]]),
+      FWD2.ReverseReads = sapply(FWD2.orients, primerHits, fn = fnRs.cut[[1]]),
+      FWD3.ForwardReads = sapply(FWD3.orients, primerHits, fn = fnFs.cut[[1]]),
+      FWD3.ReverseReads = sapply(FWD3.orients, primerHits, fn = fnRs.cut[[1]]),
+      FWD4.ForwardReads = sapply(FWD4.orients, primerHits, fn = fnFs.cut[[1]]),
+      FWD4.ReverseReads = sapply(FWD4.orients, primerHits, fn = fnRs.cut[[1]]),
+      FWD5.ForwardReads = sapply(FWD5.orients, primerHits, fn = fnFs.cut[[1]]),
+      FWD5.ReverseReads = sapply(FWD5.orients, primerHits, fn = fnRs.cut[[1]]),
+      REV.ForwardReads = sapply(REV.orients, primerHits, fn = fnFs.cut[[1]]),
+      REV.ReverseReads = sapply(REV.orients, primerHits, fn = fnRs.cut[[1]]))
+
